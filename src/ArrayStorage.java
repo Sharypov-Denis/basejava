06969593472b629edf4e9cb6fca0f5d097679be3
Sharypov-Denis <sharypov.denis@gmail.com>
2020-07-06 16:04:30 +0300

    Refactory 4

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -5,7 +5,7 @@ import java.util.Arrays;
  * Array based storage for Resumes
  */
 public class ArrayStorage {
-    Resume[] storage = new Resume[3];
+    Resume[] storage = new Resume[5];
     int size = 0;
 
 
@@ -31,35 +31,20 @@ public class ArrayStorage {
     }
 
     void delete(String uuid) {
-        Resume[] resume = new Resume[size];
-        int itemNewArray = 0;//номер элемента в новом массиве для записи
-        boolean deletedItem = false;//если будет удален элемент - то true
         for (int i = 0; i < size; i++) {
             if (storage[i].getUuid().equals(uuid)) {
-                storage[i] = null;
-                deletedItem = true;
-            } else {//если не было удаление элементы, переписываем элементы в новый массив
-                resume[itemNewArray] = storage[i];
-                itemNewArray++;//добавляем 1 к номеру,поможет если был удален элемент,
-                //чтобы в новый массив значение добавлять с самого начала без пропусков
+                storage[i] = storage[size - 1];
+                size--;
+                break;
             }
         }
-        if (deletedItem) size--;//если был удален элемент - уменьшаем кол-во резюме
-
-        storage = Arrays.stream(resume).toArray(Resume[]::new);
-
-        /*
-        storage = new Resume[size];
-        for (int i = 0; i < size; i++) {
-            storage[i] = resume[i];
-        }
-         */
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
+
         Resume[] resumeAll = new Resume[size];
         for (int i = 0; i < size; i++) {
             resumeAll[i] = storage[i];
