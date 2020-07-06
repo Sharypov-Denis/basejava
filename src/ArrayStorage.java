dce1db12e43e5da0bf4fac268335e9665beeb303
Sharypov-Denis <sharypov.denis@gmail.com>
2020-07-06 12:22:00 +0300

    Refactory 3

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -1,22 +1,16 @@
-import netscape.security.UserTarget;
-
 import java.lang.reflect.Array;
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
 
 /**
  * Array based storage for Resumes
  */
 public class ArrayStorage {
-    Resume[] storage = new Resume[10000];
+    Resume[] storage = new Resume[3];
     int size = 0;
 
 
     void clear() {
-        for (int i = 0; i < size; i++) {
-            storage[i] = null;
-        }
+        Arrays.fill(storage, 0, size, null);
         size = 0;
     }
 
@@ -26,28 +20,40 @@ public class ArrayStorage {
     }
 
     Resume get(String uuid) {
-        Resume search = null;
-        for (Resume resume : storage) {
-            if (resume.getUuid().equals(uuid)) {
-                search = resume;
+        Resume resume = null;
+        for (int i = 0; i < size; i++) {
+            if (storage[i].getUuid().equals(uuid)) {
+                resume = storage[i];
                 break;
             }
         }
-        return search;
+        return resume;
     }
 
     void delete(String uuid) {
-        boolean delete = false;
-        for (int i = 0; i < size + 1; i++) {
+        Resume[] resume = new Resume[size];
+        int itemNewArray = 0;//номер элемента в новом массиве для записи
+        boolean deletedItem = false;//если будет удален элемент - то true
+        for (int i = 0; i < size; i++) {
             if (storage[i].getUuid().equals(uuid)) {
-                storage[i] = storage[i + 1];
-                size--;
-                delete = true;
-            }
-            if (delete) {
-                storage[i] = storage[i + 1];
+                storage[i] = null;
+                deletedItem = true;
+            } else {//если не было удаление элементы, переписываем элементы в новый массив
+                resume[itemNewArray] = storage[i];
+                itemNewArray++;//добавляем 1 к номеру,поможет если был удален элемент,
+                //чтобы в новый массив значение добавлять с самого начала без пропусков
             }
         }
+        if (deletedItem) size--;//если был удален элемент - уменьшаем кол-во резюме
+
+        storage = Arrays.stream(resume).toArray(Resume[]::new);
+
+        /*
+        storage = new Resume[size];
+        for (int i = 0; i < size; i++) {
+            storage[i] = resume[i];
+        }
+         */
     }
 
     /**
