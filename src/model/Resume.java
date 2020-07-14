ce8d1023e9991c2417e5d7fd3c3950d133fd2be1
Sharypov-Denis <sharypov.denis@gmail.com>
2020-07-14 10:17:28 +0300

    HW3 - not resolved

--- src/model/Resume.java
+++ src/model/Resume.java
@@ -3,10 +3,9 @@ package model;
 /**
  * Initial resume class
  */
-public class Resume {
-
+public class Resume implements Comparable<Resume> {
     // Unique identifier
-    String uuid;
+    private String uuid;
 
     public String getUuid() {
         return uuid;
@@ -17,7 +16,27 @@ public class Resume {
     }
 
     @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Resume resume = (Resume) o;
+
+        return uuid.equals(resume.uuid);
+    }
+
+    @Override
+    public int hashCode() {
+        return uuid.hashCode();
+    }
+
+    @Override
     public String toString() {
         return uuid;
+    }
+
+    @Override
+    public int compareTo(Resume o) {
+        return uuid.compareTo(o.uuid);
     }
 }
