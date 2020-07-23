c5857542a2cce8661c3ae340a284f9f6249ce664
Sharypov-Denis <sharypov.denis@gmail.com>
2020-07-23 19:06:55 +0300

    HW8 - finish resolved

---
+++ src/MainFile.java
@@ -0,0 +1,64 @@
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+
+public class MainFile {
+    public static void main(String[] args) {
+        String filePath = ".\\.gitignore";
+        File file = new File("C:\\Intership\\basejava\\.gitignore");
+        try {
+            System.out.println(file.getCanonicalPath());
+        } catch (IOException e) {
+            throw new RuntimeException("Error", e);
+        }
+        File dir = new File("C:\\Intership\\basejava\\src");
+        System.out.println("проверка isDirectory" + dir.isDirectory());
+        String[] list = dir.list();
+        if (list != null) {
+            for (String name : dir.list()) {//вывод всех файлов
+                System.out.println(name);
+            }
+        }
+        try (FileInputStream fis = new FileInputStream(filePath)) {
+            System.out.println(fis.read());
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        printDirectoryDeeply(dir);
+    }
+    // TODO: make pretty output
+    public static void printDirectoryDeeply(File dir) {
+        File[] files = dir.listFiles();
+
+        if (files != null) {
+            for (File file : files) {
+                if (file.isFile()) {
+                    System.out.println("File: " + file.getName());
+                } else if (file.isDirectory()) {
+                    System.out.println("Directory: " + file.getName());
+                    printDirectoryDeeply(file);
+                }
+            }
+        }
+    }
+}
+
+//ниже сложная схема
+        /*
+        FileInputStream fis = null;
+        try {
+            fis = new FileInputStream(filePath);
+            System.out.println(fis.read());
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        } finally {
+            if (file != null) {
+                try {
+                    fis.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+         */
